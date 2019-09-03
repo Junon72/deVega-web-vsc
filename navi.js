c3bc10adaa7535371731e499ad17afeec04269a9
Jussi Nousiainen <juno.athome@gmail.com>
2019-09-03 14:36:04 +0200

    js for navigation bar

---
+++ navi.js
@@ -0,0 +1,17 @@
+/* navbar toggler behaviour - from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_right */
+function navToggle() {
+    document.getElementById("collapseMenu").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('navbar-toggler')) {
+        var dropdowns = document.getElementsByClassName("navbar-collapse");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns [i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show')
+            }
+        }
+    }
+}
