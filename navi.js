5ff406d6347dc85ae5d82e849b9d23bc568fff1d
Jussi Nousiainen <juno.athome@gmail.com>
2019-09-09 23:19:33 +0200

    beautified/ added java to progress pages

--- navi.js
+++ navi.js
@@ -8,7 +8,7 @@ window.onclick = function (event) {
         var dropdowns = document.getElementsByClassName("navbar-collapse");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns [i];
+            var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show')
             }
